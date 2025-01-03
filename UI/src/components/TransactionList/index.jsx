08cbd5389f74079b295a02574103f0fe175a9693
WASPEi2 <aekapab10@gmail.com>
2025-01-03 16:44:45 +0700

    enhance TransactionList display

--- UI/src/components/TransactionList/index.jsx
+++ UI/src/components/TransactionList/index.jsx
@@ -2,35 +2,53 @@ import React from "react";
 
 const TransactionList = ({ transactions, removeTransaction }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Type</th>
-          <th>Title</th>
-          <th>Amount</th>
-          <th>Date</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map((transaction) => (
-          <tr key={transaction.id}>
-            <td>{transaction.id}</td>
-            <td>{transaction.type}</td>
-            <td>{transaction.title}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.transaction_date}</td>
-            <td>
-              <button onClick={() => removeTransaction(transaction.id)}>
-                Delete
-              </button>
-              {/* You could add edit functionality here */}
-            </td>
+    <div>
+      <table className="table table-striped table-bordered">
+        <thead className="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Type</th>
+            <th>Title</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+            <th>Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.id}</td>
+              <td>{transaction.type}</td>
+              <td>{transaction.title}</td>
+              <td>{transaction.amount}</td>
+
+              {/* fix date format to DD/MM/YYY for all device */}
+              <td>
+                {new Date(transaction.transaction_date).toLocaleDateString(
+                  "en-GB"
+                )}
+              </td>
+              <td>
+                {new Date(transaction.created_at).toLocaleDateString("en-GB")}
+              </td>
+              <td>
+                {new Date(transaction.updated_at).toLocaleDateString("en-GB")}
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => removeTransaction(transaction.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
